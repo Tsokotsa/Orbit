ce99016b56c4bfe3924ff49d849f6f59c9058e0a
Stelio Timane <stimane@Stelios-MacBook-Pro.local>
2026-02-21 13:24:21 +0200

    Adding Midleware Auth to Routes

--- routes/calix.php
+++ routes/calix.php
@@ -2,12 +2,12 @@
 
 use App\Http\Controllers\CalixController;
 
+Route::middleware('auth')->group(function () {
 
+    Route::get('/calix', action: [CalixController::class, 'index'])->name('calix_dashbard');
+    Route::get(uri: '/calix/subscribers/list', action: [CalixController::class, 'list_subscribers'])->name('list.all-subscribers');
+    Route::get(uri: '/calix/subscribers', action: [CalixController::class, 'get_all_subscribers'])->name('subscribers.datatable');
+    Route::get(uri: '/calix/subscribers/{customID}', action: [CalixController::class, 'view_subscriber'])->name('view.subscriber');
 
-Route::get('/calix', action: [CalixController::class, 'index'])->name('calix_dashbard');
-Route::get(uri: '/calix/subscribers/list', action: [CalixController::class, 'list_subscribers'])->name('list.all-subscribers');
-Route::get(uri: '/calix/subscribers', action: [CalixController::class, 'get_all_subscribers'])->name('subscribers.datatable');
-Route::get(uri: '/calix/subscribers/{customID}', action: [CalixController::class, 'view_subscriber'])->name('view.subscriber');
-
-
+});
 
--- routes/odoo.php
+++ routes/odoo.php
@@ -4,11 +4,13 @@ use App\Http\Controllers\ClientController;
 use App\Http\Controllers\OdooController;
 use App\Http\Controllers\ServiceController;
 
+Route::middleware('auth')->group(function () {
 
-Route::get('/billing', [OdooController::class, 'billing'])->name('billing.view');
-Route::get('/billing/list', [OdooController::class, 'get_all_ajax'])->name('billing.getAjax');
+    Route::get('/billing', [OdooController::class, 'billing'])->name('billing.view');
+    Route::get('/billing/list', [OdooController::class, 'get_all_ajax'])->name('billing.getAjax');
+    Route::get('/client/sales-order', [OdooController::class, 'sales_order'])->name('view.sales.orders');
+});
 
-Route::get('/client/sales-order', [OdooController::class, 'sales_order'])->name('view.sales.orders');
 
 // TESTE
 Route::get('/odoo/quotes', [OdooController::class, 'quotes']);
